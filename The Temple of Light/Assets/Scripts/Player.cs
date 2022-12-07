fb7e249fc6ec41ef67fe777378efa4f6cc3f6697
Akshat Chhabra <akshatchhabra@Akshats-MacBook-Air.local>
2022-12-07 12:39:16 -0500

    fixing player movement

--- The Temple of Light/Assets/Scripts/Player.cs
+++ The Temple of Light/Assets/Scripts/Player.cs
@@ -6,13 +6,15 @@ public class Player : MonoBehaviour
 {
     private Animator animation_controller;
     private CharacterController character_controller;
+    private float turn_smooth_velocity;
+    private bool is_close_to_column;
 
     public Vector3 movement_direction;
-    public float walking_velocity;
     public float velocity;
     public Vector3 lrot;
     public Vector3 rrot;
-    public float rotationSpeed;
+    public float rotation_speed;
+    public float turn_smooth_time;
     
     // Start is called before the first frame update
     void Start()
@@ -20,36 +22,30 @@ public class Player : MonoBehaviour
         animation_controller = GetComponent<Animator>();
         character_controller = GetComponent<CharacterController>();
         movement_direction = new Vector3(0.0f, 0.0f, 0.0f);
-        walking_velocity = 1.0f;
-        velocity = 1.0f;
+        velocity = 3.0f;
         lrot = new Vector3(0.0f, -1.0f, 0.0f);
         rrot = new Vector3(0.0f, 1.0f, 0.0f);
-        rotationSpeed = 50.0f;
-        
+        rotation_speed = 50.0f;
+        turn_smooth_time = 0.1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool is_walking_forward = Input.GetKey("w") || Input.GetKey("up");
-        animation_controller.SetBool("is_walking_forward", is_walking_forward);
-        bool is_walking_back = Input.GetKey("s") || Input.GetKey("down");
-        animation_controller.SetBool("is_walking_back", is_walking_back);
-        movement_direction = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-
-        if (is_walking_forward){
+        float horizontal_axis = Input.GetAxis("Horizontal");
+        float vertical_axis = Input.GetAxis("Vertical");
+        movement_direction = new Vector3(horizontal_axis, 0.0f, vertical_axis);
+        movement_direction.Normalize();
+        bool is_walking = Input.GetKey("w") || Input.GetKey("up")
+                            || Input.GetKey("a") || Input.GetKey("left") 
+                            || Input.GetKey("s") || Input.GetKey("down")
+                            || Input.GetKey("d") || Input.GetKey("right") ;
+        animation_controller.SetBool("is_walking", is_walking);
+        float target_angle = Mathf.Atan2(movement_direction.x, movement_direction.z) * Mathf.Rad2Deg;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target_angle, ref turn_smooth_velocity, turn_smooth_time);
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        if (movement_direction.magnitude >= 0.1f){
             character_controller.Move(movement_direction * velocity * Time.deltaTime);
-        }
-        if (is_walking_back){
-            character_controller.Move(-1 * movement_direction * velocity * Time.deltaTime);
-        }
-        if (Input.GetKey("left") || Input.GetKey("a"))
-        {
-            transform.Rotate(lrot * rotationSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey("right") || Input.GetKey("d"))
-        {
-            transform.Rotate(rrot * rotationSpeed * Time.deltaTime);
         }
     }
 }
