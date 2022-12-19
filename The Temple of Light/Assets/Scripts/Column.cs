f4c7bb1579c4b65c0a84bb3cf6623e257e325acc
LyraDusk <lauren@meyerzone.net>
2022-12-19 18:13:22 -0500

    Column Orientation fixes

--- The Temple of Light/Assets/Scripts/Column.cs
+++ The Temple of Light/Assets/Scripts/Column.cs
@@ -15,16 +15,6 @@ public enum ColType {
 
 }
 
-public enum LightColor { // where any light color is the sum of its parts
-  NONE = 0,
-  RED = 1,        // basic colors are powers of 2, others are sums of them
-  GREEN = 2,      // and white is the sum of all three
-  YELLOW = 3,     // This can be moved to whichever classes handle light if it's easier
-  BLUE = 4,
-  MAGENTA = 5,
-  CYAN = 6,
-  WHITE = 7,
-}
 
 public class Column : MonoBehaviour
 {
@@ -39,14 +29,8 @@ public class Column : MonoBehaviour
     public GameObject player_reference;
     public GameObject lightObject;
 
-    internal bool is_lit;             // whether or not the column is receiving a light beam
-    internal int lit_angle;           // logical angle the light is coming from (0-7), -1 if not lit
-    internal int out_angle;           // if mirrored, the angle incoming light gets reflected
-    internal int through_angle;       // if clear & not lens, angle of nonreflected light
-    internal bool blocking;           // Is it blocking the light? (wrong incoming angle, etc)
-    internal LightColor lit_color;         // color of incoming light. "NONE" if not lit.
-    internal LightColor out_color;         // color of outgoing reflected light, "NONE" if not lit.
-    internal LightColor through_color;     // color of light passing through, "NONE" if not lit or wrong color in
+    public bool is_lit;             // whether or not the column is receiving a light beam
+    internal Color lit_color;         // color of incoming light. "NONE" if not lit.
         // Remaining light handling can be done in the actual light class (or all of it, if you want)
 
     private bool being_carried;
@@ -56,20 +40,16 @@ public class Column : MonoBehaviour
     void Start()
     {
       is_lit = false;
-      lit_angle = -1;
-      out_angle = -1;
-      blocking = false;
-      lit_color = LightColor.NONE;
-      out_color = LightColor.NONE;
+      lit_color = Color.black;
       if((int)type < 7) {
         movable = true;
       }
       being_carried = false;
-      facing = (int)((transform.eulerAngles.y - 90) / 45f);
+      facing = (((int)transform.eulerAngles.y + 1) / 45);
       if(type == ColType.LIGHT_EMIT)
-        facing = (int)((transform.eulerAngles.y + 90) / 45f) % 8;
+        facing = (((int)transform.eulerAngles.y + 91) / -45) % 8;
       if(type == ColType.LIGHT_RECV)
-        facing = (int)((transform.eulerAngles.y + 180) / 45f) % 8;
+        facing = (((int)transform.eulerAngles.y + 181) / -45) % 8;
       facing_angle = new Angle(facing);
       if (type == ColType.LIGHT_EMIT)
       {
@@ -84,6 +64,11 @@ public class Column : MonoBehaviour
       if(being_carried) {
         transform.position = player_reference.transform.position + new Vector3(0f,2f,0f);
       }
+      //DEBUG
+      if(Input.GetKeyDown("r")) {
+        RotateCol();
+      }
+
 
     }
 
